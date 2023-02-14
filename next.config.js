d1f75f2f8d7c8ba62ca2b6d0f1a04f9d6d44da4d
ChrisCooper0 <christopher.cooper@mail.com>
2023-02-14 08:18:07 +0000

    first draft

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    CALLINK: "chriscooper/30min",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Cal, { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
+import { FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
 
 export default function Home() {
+  useEffect(() => {
+    (async function () {
+      const cal: any = await getCalApi();
+      cal("ui", {
+        styles: { branding: { brandColor: "#06b6d4" } },
+        hideEventTypeDetails: false,
+      });
+    })();
+  }, []);
   return (
     <>
       <Head>
@@ -14,109 +22,43 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
+      <main className="flex flex-col justify-center items-center gap-14 w-full h-full mt-10 md:mt-16 px-5">
+        <div className="flex flex-col md:flex-row justify-center items-center gap-8">
+          <img
+            alt="profile picture"
+            src="/chris.jpeg"
+            className="rounded-full w-32 h-32 outline-1 outline-cyan-500 outline"
           />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
+          <div className="flex flex-col gap-4">
+            <h1 className="text-3xl font-medium md:text-4xl text-slate-200">
+              Chris Cooper
+            </h1>
+            <div className="flex flex-col gap-1.5">
+              <p className="font-extralight text-slate-100">
+                Hey, I&apos;m Chris 👋
+              </p>
+              <p className="font-extralight text-slate-100">
+                I&apos;m a Software Developer, primarily focussed on the
+                front-end.
+              </p>
+              <p className="font-extralight text-slate-100">
+                Book in a call with me today!
+              </p>
+              <div className="flex flex-row gap-2">
+                <a href="https://www.linkedin.com/in/chris--cooper/">
+                  <FaLinkedin color="#0077b5" size={28} />
+                </a>
+                <a href="mailto:christopher.cooper@mail.com">
+                  <HiOutlineMail color="whitesmoke" size={30} />
+                </a>
+              </div>
+            </div>
           </div>
         </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        <Cal
+          calLink={process.env.CALLINK ?? ""}
+          style={{ width: "100%", height: "100%", overflow: "hidden" }}
+        />
       </main>
     </>
   );
