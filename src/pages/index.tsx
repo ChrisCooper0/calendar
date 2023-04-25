06a4d883e610f0936793ae1661b4207a1d82480a
ChrisCooper0 <christopher.cooper@mail.com>
2023-04-25 10:59:56 +0100

    button copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -90,7 +90,7 @@ export default function Home() {
             data-commerce
             rel="noreferrer"
           >
-            CV Template
+            My CV Template
           </a>
         </div>
         <Cal
